92f825fdf34e9b74b054c707ffef181bdcf6263b
Harsh19062002 <harshmahajan1962002@gmail.com>
2026-04-08 11:46:44 +0530

    updates

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,3 +1,6 @@
+import genToken from "../config/token.js"
+import User from "../models/user.model.js"
+
 export const googleAuth = async (req,res) => {
     try {
         const { name, email } = req.body;
@@ -28,3 +31,21 @@ export const googleAuth = async (req,res) => {
         return res.status(500).json({ message: error.message });
     }
 };
+
+export const logOut = async (req,res) => {
+    try {
+
+        res.clearCookie("token", {
+            httpOnly: true,
+            sameSite: "lax",
+            secure: false
+        })
+
+        return res.status(200).json({
+            message:"Logout Successfully"
+        })
+
+    } catch (error) {
+        return res.status(500).json({message:`Logout error ${error}`})
+    }
+}
